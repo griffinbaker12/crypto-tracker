27c0aac080e683e3d794ef832c5298333b99b7cc
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-02-16 23:59:12 -0600

    ok

---
+++ app/get_price.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+from enums import Coin
+
+code_to_price = {}
+
+
+def get_crytpo_price(code: Coin):
+    url = "https://binance.com"
+    print(url)
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    price_div = soup.find_all("div", {"class": "coinRow-coinPrice css-1ld3mhe"})
+    for div in price_div:
+        parent = div.find_parent(id=lambda x: x and "top_crypto_table" in x)
+        coin_code = parent.find("div", {"class": "coinRow-coinCode"}).text
+        coin_price = div.text
+        code_to_price[coin_code] = coin_price
+
+    coin_name = code.name
+    if coin_name in code_to_price:
+        return code_to_price[coin_name]
+    else:
+        return "No price found"
+
+
+print(get_crytpo_price(Coin.BTC))
---
+++ app/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from enums import Coin
+from get_price import get_crytpo_price
+
+app = FastAPI()
+
+# CORS middleware to allow cross-origin requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow requests from any origin, adjust as needed
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS"],  # Allow these HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/hello")
+def read_hello():
+    return {"Hello": "World"}
+
+
+@app.get("/coin/{coin}")
+def read_coin(coin: Coin):
+    price = get_crytpo_price(coin)
+    return {"coin": coin, "price": price}
